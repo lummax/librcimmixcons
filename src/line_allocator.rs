0cadeed05879c59c537926b10661de3623bbeea7
lummax <luogpg@googlemail.com>
2015-01-05 22:15:16 +0100

    Reorganize methods for LineAllocator

--- src/line_allocator.rs
+++ src/line_allocator.rs
@@ -51,6 +51,18 @@ impl LineAllocator {
         return self.object_map.contains(&object);
     }
 
+    pub fn current_live_mark(&self) -> bool {
+        return self.current_live_mark;
+    }
+
+    pub fn decrement_lines(&mut self, object: GCObjectRef) {
+        unsafe{ (*self.get_block_ptr(object)).decrement_lines(object); }
+    }
+
+    pub fn increment_lines(&mut self, object: GCObjectRef) {
+        unsafe{ (*self.get_block_ptr(object)).increment_lines(object); }
+    }
+
     pub fn allocate(&mut self, rtti: *const GCRTTI) -> Option<GCObjectRef> {
         let size = unsafe{ (*rtti).object_size() };
         debug!("Request to allocate an object of size {}", size);
@@ -59,6 +71,70 @@ impl LineAllocator {
             return Some(object);
         }
         return None;
+    }
+
+    pub fn prepare_collection(&mut self) -> bool {
+        self.unavailable_blocks.extend(self.recyclable_blocks.drain());
+        self.unavailable_blocks.extend(self.current_block.take()
+                                           .map(|b| b.0).into_iter());
+
+        let perform_cycle_collection = true;
+        return perform_cycle_collection;
+    }
+
+    pub fn complete_collection(&mut self) {
+        self.mark_histogram.clear();
+        let mut unavailable_blocks = RingBuf::new();
+        for block in self.unavailable_blocks.drain() {
+            if unsafe{ (*block).is_empty() } {
+                debug!("Return block {:p} to global block allocator", block);
+                self.block_allocator.return_block(block);
+            } else {
+                let (holes, marked_lines) = unsafe{ (*block).count_holes_and_marked_lines() };
+                self.mark_histogram.update(holes as uint, marked_lines, |o, n| o + n);
+                debug!("Found {} holes and {} marked lines in block {}",
+                       holes, marked_lines, block);
+                match holes {
+                    0 => {
+                        debug!("Push block {:p} into unavailable_blocks", block);
+                        unavailable_blocks.push_back(block);
+                    },
+                    _ => {
+                        debug!("Push block {:p} into recyclable_blocks", block);
+                        self.recyclable_blocks.push_back(block);
+                    }
+                }
+            }
+        }
+        self.unavailable_blocks.extend(unavailable_blocks.into_iter());
+
+        if cfg!(feature = "valgrind") {
+            for object in self.object_map_backup.drain() {
+                valgrind_freelike!(object);
+            }
+        }
+    }
+
+    pub fn prepare_immix_collection(&mut self) {
+        for block in self.unavailable_blocks.iter_mut() {
+            unsafe{ (**block).clear_line_counts(); }
+        }
+
+        if cfg!(feature = "valgrind") {
+            self.object_map_backup = self.object_map.clone();
+        }
+        self.object_map.clear();
+    }
+
+    pub fn complete_immix_collection(&mut self) {
+        self.current_live_mark = !self.current_live_mark;
+    }
+}
+
+impl LineAllocator {
+    unsafe fn get_block_ptr(&mut self, object: GCObjectRef) -> *mut BlockInfo {
+        let block_offset = object as uint % BLOCK_SIZE;
+        return mem::transmute((object as *mut u8).offset(-(block_offset as int)));
     }
 
     fn raw_allocate(&mut self, size: uint) -> Option<GCObjectRef> {
@@ -131,78 +207,4 @@ impl LineAllocator {
             object, size, block, size >= LINE_SIZE);
             return ((block, low + size as u16, high), object);
         }
-
-    pub fn prepare_collection(&mut self) -> bool {
-        self.unavailable_blocks.extend(self.recyclable_blocks.drain());
-        self.unavailable_blocks.extend(self.current_block.take()
-                                           .map(|b| b.0).into_iter());
-
-        let perform_cycle_collection = true;
-        return perform_cycle_collection;
-    }
-
-    pub fn complete_collection(&mut self) {
-        self.mark_histogram.clear();
-        let mut unavailable_blocks = RingBuf::new();
-        for block in self.unavailable_blocks.drain() {
-            if unsafe{ (*block).is_empty() } {
-                debug!("Return block {:p} to global block allocator", block);
-                self.block_allocator.return_block(block);
-            } else {
-                let (holes, marked_lines) = unsafe{ (*block).count_holes_and_marked_lines() };
-                self.mark_histogram.update(holes as uint, marked_lines, |o, n| o + n);
-                debug!("Found {} holes and {} marked lines in block {}",
-                       holes, marked_lines, block);
-                match holes {
-                    0 => {
-                        debug!("Push block {:p} into unavailable_blocks", block);
-                        unavailable_blocks.push_back(block);
-                    },
-                    _ => {
-                        debug!("Push block {:p} into recyclable_blocks", block);
-                        self.recyclable_blocks.push_back(block);
-                    }
-                }
-            }
-        }
-        self.unavailable_blocks.extend(unavailable_blocks.into_iter());
-
-        if cfg!(feature = "valgrind") {
-            for object in self.object_map_backup.drain() {
-                valgrind_freelike!(object);
-            }
-        }
-    }
-
-    pub fn prepare_immix_collection(&mut self) {
-        for block in self.unavailable_blocks.iter_mut() {
-            unsafe{ (**block).clear_line_counts(); }
-        }
-
-        if cfg!(feature = "valgrind") {
-            self.object_map_backup = self.object_map.clone();
-        }
-        self.object_map.clear();
-    }
-
-    pub fn complete_immix_collection(&mut self) {
-        self.current_live_mark = !self.current_live_mark;
-    }
-
-    unsafe fn get_block_ptr(&mut self, object: GCObjectRef) -> *mut BlockInfo {
-        let block_offset = object as uint % BLOCK_SIZE;
-        return mem::transmute((object as *mut u8).offset(-(block_offset as int)));
-    }
-
-    pub fn decrement_lines(&mut self, object: GCObjectRef) {
-        unsafe{ (*self.get_block_ptr(object)).decrement_lines(object); }
-    }
-
-    pub fn increment_lines(&mut self, object: GCObjectRef) {
-        unsafe{ (*self.get_block_ptr(object)).increment_lines(object); }
-    }
-
-    pub fn current_live_mark(&self) -> bool {
-        return self.current_live_mark;
-    }
 }
