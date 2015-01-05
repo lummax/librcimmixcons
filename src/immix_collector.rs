423deed3451a804f4c1222da3d4258b90c5f1158
lummax <luogpg@googlemail.com>
2015-01-05 22:50:02 +0100

    Implement pinning of roots in ImmixCollector

--- src/immix_collector.rs
+++ src/immix_collector.rs
@@ -13,22 +13,31 @@ impl ImmixCollector {
         let next_live_mark = !line_allocator.current_live_mark();
         debug!("Start Immix collection with {} roots and next_live_mark: {}",
                roots.len(), next_live_mark);
-        let mut object_queue = roots.iter().map(|o| *o)
-                                    .collect::<RingBuf<GCObjectRef>>();
         line_allocator.prepare_immix_collection();
+        let mut object_queue = RingBuf::new();
+        for root in roots.iter().map(|o| *o) {
+            unsafe{ (*root).set_pinned(true); }
+            object_queue.push_back(root);
+        }
         while let Some(object) =  object_queue.pop_front() {
             debug!("Process object {} in Immix closure", object);
             if !unsafe { (*object).set_marked(next_live_mark) } {
-                debug!("Object {} was unmarked: process children", object);
-                line_allocator.set_gc_object(object);
-                line_allocator.increment_lines(object);
-                for child in unsafe{ (*object).children() }.into_iter() {
-                    if !unsafe{ (*child).is_marked(next_live_mark) } {
-                        debug!("Push child {} into object queue", child);
-                        object_queue.push_back(child);
+                debug!("Process object {} in Immix closure", object);
+                if !unsafe { (*object).set_marked(next_live_mark) } {
+                    debug!("Object {} was unmarked: process children", object);
+                    line_allocator.set_gc_object(object);
+                    line_allocator.increment_lines(object);
+                    for child in unsafe{ (*object).children() }.into_iter() {
+                        if !unsafe{ (*child).is_marked(next_live_mark) } {
+                            debug!("Push child {} into object queue", child);
+                            object_queue.push_back(child);
+                        }
                     }
                 }
             }
+        }
+        for root in roots.iter() {
+            unsafe{ (**root).set_pinned(false); }
         }
         line_allocator.complete_immix_collection();
         debug!("Complete collection");
