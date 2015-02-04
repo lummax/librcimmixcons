5dbde85a16e0c4f34162fdb7ed7b87af0d4b19c3
lummax <luogpg@googlemail.com>
2015-02-04 11:42:56 +0100

    Add rustdoc for allocator/mod.rs

--- src/spaces/immix_space/allocator/mod.rs
+++ src/spaces/immix_space/allocator/mod.rs
@@ -15,18 +15,40 @@ use std::collections::RingBuf;
 use constants::LINE_SIZE;
 use gc_object::GCObjectRef;
 
+/// A type alias for the block, the current low and high offset.
 type BlockTuple = (*mut BlockInfo, u16, u16);
 
+/// Trait for the allocators in the immix space.
+///
+/// Only use `get_all_blocks()` and `allocate()` from outside.
 pub trait Allocator {
+
+    /// Get all block managed by the allocator, draining any local
+    /// collections.
     fn get_all_blocks(&mut self) -> RingBuf<*mut BlockInfo>;
 
+    /// Get the current block to allocate from.
     fn take_current_block(&mut self) -> Option<BlockTuple>;
+
+    /// Set the current block to allocate from.
     fn put_current_block(&mut self, block_tuple: BlockTuple);
 
+    /// Get a new block from a block resource.
     fn get_new_block(&mut self) -> Option<BlockTuple>;
+
+    /// Callback if no hole of `size` bytes was found in the current block.
     fn handle_no_hole(&mut self, size: usize) -> Option<BlockTuple>;
+
+    /// Callback if the given `block` has no holes left.
     fn handle_full_block(&mut self, block: *mut BlockInfo);
 
+    /// Allocate an object of `size` bytes or return `None`.
+    ///
+    /// This object is not initialized, just the memory chunk is allocated.
+    ///
+    /// This will try to find a hole in the `take_current_block()`. If there
+    /// Is no hole `handle_no_hole()` will be called. If this function returns
+    /// `None` a 'get_new_block()' is requested.
     fn allocate(&mut self, size: usize) -> Option<GCObjectRef> {
         debug!("Request to allocate an object of size {}", size);
         return self.take_current_block()
@@ -41,6 +63,11 @@ pub trait Allocator {
                    });
     }
 
+    /// Scan a block tuple for a hole of `size` bytes and return a matching
+    /// hole.
+    ///
+    /// If no hole was found `handle_full_block()` is called and None
+    /// returned.
     fn scan_for_hole(&mut self, size: usize, block_tuple: BlockTuple) -> Option<BlockTuple> {
         let (block, low, high) = block_tuple;
         return match (high - low) as usize >= size {
@@ -58,6 +85,13 @@ pub trait Allocator {
         };
     }
 
+    /// Allocate an uninitialized object of `size` bytes from the block tuple.
+    ///
+    /// Returns the block tuple with a modified low offset and the allocated
+    /// object pointer.
+    ///
+    /// _Note_: This must only be called if there is a hole of `size` bytes
+    /// starting at low offset!
     fn allocate_from_block(&self, size: usize, block_tuple: BlockTuple)
         -> (BlockTuple, GCObjectRef) {
             let (block, low, high) = block_tuple;
