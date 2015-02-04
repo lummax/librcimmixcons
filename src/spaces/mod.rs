6527e906eab97a22cfd46dedba03fc13b70a4f39
lummax <luogpg@googlemail.com>
2015-02-04 14:05:12 +0100

    Add rustdoc for spaces/mod.rs

--- src/spaces/mod.rs
+++ src/spaces/mod.rs
@@ -13,14 +13,26 @@ use constants::LARGE_OBJECT;
 use gc_object::{GCRTTI, GCObjectRef};
 use stack;
 
+/// The type of collection that will be performed.
 pub enum CollectionType {
+    /// A simple reference counting collection.
     RCCollection,
+
+    /// A reference counting collection with proactive opportunistic
+    /// evacuation.
     RCEvacCollection,
+
+    /// A reference counting collection followed by the immix tracing (cycle)
+    /// collection.
     ImmixCollection,
+
+    /// A reference counting collection followed by the immix tracing (cycle)
+    /// collection. Both with opportunistict evacuation.
     ImmixEvacCollection,
 }
 
 impl CollectionType {
+    /// Returns if this `CollectionType` is an evacuating collection.
     pub fn is_evac(&self) -> bool {
         use self::CollectionType::{RCEvacCollection, ImmixEvacCollection};
         return match *self {
@@ -29,6 +41,7 @@ impl CollectionType {
         }
     }
 
+    /// Returns if this `CollectionType` is a cycle collecting collection.
     pub fn is_immix(&self) -> bool {
         use self::CollectionType::{ImmixCollection, ImmixEvacCollection};
         return match *self {
@@ -38,14 +51,30 @@ impl CollectionType {
     }
 }
 
+/// The `Spaces` contains the different garbage collector spaces in which
+/// objects can be allocated that are managed by some collector.
 pub struct Spaces {
+    /// The default immix space.
     immix_space: ImmixSpace,
+
+    /// The large object space for objects greater than `LARGE_OBJECT`.
     large_object_space: LargeObjectSpace,
+
+    /// The collectors.
     collector: Collector,
+
+    /// The current live mark.
+    ///
+    /// During allocation of objects this value is used as the `mark` state of
+    /// new objects. During allocation the value is negated and used to mark
+    /// objects during the tracing mark phase. This way the newly allocated
+    /// objects are always initialized with the last `mark` state with will be
+    /// flipped if they are reached is the mark phase.
     current_live_mark: bool,
 }
 
 impl Spaces {
+    /// Create a new `Spaces`.
     pub fn new() -> Spaces {
         return Spaces {
             immix_space: ImmixSpace::new(),
@@ -55,17 +84,21 @@ impl Spaces {
         };
     }
 
+    /// Return if the given address is valid in any of the managed spaces.
     pub fn is_gc_object(&self, object: GCObjectRef) -> bool {
         return self.immix_space.is_gc_object(object)
                || self.large_object_space.is_gc_object(object);
     }
 
+    /// A write barrier for the given `object` used with the `RCCollector`.
     pub fn write_barrier(&mut self, object: GCObjectRef) {
         if self.is_gc_object(object) {
             self.collector.write_barrier(object);
         }
     }
 
+    /// Allocate a new object described by the `rtti` or returns `None` if
+    /// there is no memory left to fullfill the allocation request.
     pub fn allocate(&mut self, rtti: *const GCRTTI) -> Option<GCObjectRef>{
         let size = unsafe{ (*rtti).object_size() };
         debug!("Request to allocate an object of size {}", size);
@@ -73,6 +106,11 @@ impl Spaces {
                else { self.large_object_space.allocate(rtti) };
     }
 
+    /// Trigger a garbage collection.
+    ///
+    /// This will always run the referece counting collector. If `evacuation`
+    /// is set the collectors will try to evacuate. If `cycle_collect` is set
+    /// the immix tracing collector will be used.
     pub fn collect(&mut self, evacuation: bool, cycle_collect: bool) {
         debug!("Requested collection (evacuation={}, cycle_collect={})",
                evacuation, cycle_collect);
