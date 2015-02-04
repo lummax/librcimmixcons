f3b87b73ef51d8371e8d4c45f113c8fa95ee2a00
lummax <luogpg@googlemail.com>
2015-02-04 11:01:49 +0100

    Add rustdoc for block_allocator.rs

--- src/spaces/immix_space/block_allocator.rs
+++ src/spaces/immix_space/block_allocator.rs
@@ -9,14 +9,38 @@ use spaces::immix_space::block_info::BlockInfo;
 use constants::{BLOCK_SIZE, HEAP_SIZE};
 use gc_object::GCObjectRef;
 
+/// The `BlockAllocator` is the global resource for blocks for the immix
+/// space.
+///
+/// On initialization it will allocate a memory map of `HEAP_SIZE` and align
+/// it to `BLOCK_SIZE`. During normal runtime it will allocate blocks on the
+/// fly from this memory map and store returned blocks in a list.
+///
+/// Blocks from this `BlockAllocator` are always aligned to `BLOCK_SIZE`.
+///
+/// The list of returned free blocks is a stack. The `BlockAllocator` will
+/// first exhaust the returned free blocks and then fall back to allocating
+/// new blocks from the memory map. This means it will return recently
+/// returned blocks first.
 pub struct BlockAllocator {
+    /// The memory map of `HEAP_SIZE`.
     mmap: os::MemoryMap,
+
+    /// The pointer to the last allocated block.
     data: *mut u8,
+
+    /// The upper bound of the memory map as a pointer.
     data_bound: *mut u8,
+
+    /// A list of returned (free) blocks.
     free_blocks: Vec<*mut BlockInfo>,
 }
 
 impl BlockAllocator {
+    /// Create a new `BlockAllocator`.
+    ///
+    /// This will `panic` if no memory map of size `HEAP_SIZE` can be
+    /// allocared.
     pub fn new() -> BlockAllocator {
         let mmap = os::MemoryMap::new(HEAP_SIZE + BLOCK_SIZE,
                                       &[os::MapOption::MapReadable,
@@ -38,30 +62,38 @@ impl BlockAllocator {
         };
     }
 
+    /// Get a new block aligned to `BLOCK_SIZE`.
     pub fn get_block(&mut self) -> Option<*mut BlockInfo> {
         return self.free_blocks.pop().or_else(|| self.build_next_block());
     }
 
+    /// Return a collection of blocks.
     pub fn return_blocks(&mut self, blocks: RingBuf<*mut BlockInfo>) {
         self.free_blocks.extend(blocks.into_iter());
     }
 
+    /// Return the total number of possible blocks.
     pub fn total_blocks(&self) -> usize {
         return HEAP_SIZE / BLOCK_SIZE;
     }
 
+    /// Return the number of unallocated blocks.
     pub fn available_blocks(&self) -> usize {
         return (((self.data_bound as usize) - (self.data as usize)) % BLOCK_SIZE)
             + self.free_blocks.len();
     }
 
+    /// Return if an address is within the bounds of the memory map.
     pub fn is_in_space(&self, object: GCObjectRef) -> bool {
         return self.mmap.data() < (object as *mut u8)
             && (object as *mut u8) < self.data_bound;
     }
 }
 
-impl BlockAllocator{
+impl BlockAllocator {
+    /// Build a new block from the memory map.
+    ///
+    /// Returns `None` if the memory map is exhausted.
     fn build_next_block(&mut self) -> Option<*mut BlockInfo> {
         let block = unsafe{ self.data.offset(BLOCK_SIZE as isize) };
         if block < self.data_bound {
