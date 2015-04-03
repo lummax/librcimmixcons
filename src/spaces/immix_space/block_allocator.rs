cdedd94254238a6d7ca397984581c4eef3706244
lummax <luogpg@googlemail.com>
2015-04-03 12:04:08 +0200

    Implement MemoryMap wrapper in block_allocator

--- src/spaces/immix_space/block_allocator.rs
+++ src/spaces/immix_space/block_allocator.rs
@@ -3,11 +3,63 @@
 
 #![allow(deprecated)]
 use std::{ptr, os};
+use libc;
 
 use spaces::immix_space::block_info::BlockInfo;
 
-use constants::{BLOCK_SIZE, HEAP_SIZE};
+use constants::{BLOCK_SIZE, HEAP_SIZE, TOTAL_BLOCKS};
 use gc_object::GCObjectRef;
+
+/// A simple wrapper for a heap mmap.
+struct MemoryMap{
+    /// The pointer to the mmap'ed region.
+    mmap: *mut libc::c_void,
+}
+
+impl MemoryMap {
+    /// Create a new `MemoryMap` of `HEAP_SIZE + BLOCK_SIZE` to be aligned to
+    /// `BLOCK_SIZE` boundaries.
+    fn new() -> MemoryMap {
+        let mmap = unsafe {
+            libc::mmap(ptr::null_mut(), (HEAP_SIZE + BLOCK_SIZE) as libc::size_t,
+                       libc::PROT_READ | libc::PROT_WRITE,
+                      libc::MAP_PRIVATE | libc::MAP_ANON, -1, 0)
+        };
+
+        if mmap == libc::MAP_FAILED {
+            panic!("Failed to allocate the heap memory map");
+        }
+
+        return MemoryMap {
+            mmap: mmap,
+        }
+    }
+
+    /// Return a `BLOCK_SIZE` aligned pointer to the mmap'ed region.
+    fn aligned(&self) -> *mut u8 {
+        let offset = BLOCK_SIZE - (self.mmap as usize) % BLOCK_SIZE;
+        return unsafe{ self.mmap.offset(offset as isize) } as *mut u8;
+    }
+
+    /// Return a pointer to the start of the mmap'ed region.
+    fn start(&self) -> *mut u8 {
+        return self.mmap as *mut u8;
+    }
+
+    /// Return a pointer to the end of the mmap'ed region.
+    fn bound(&self) -> *mut u8 {
+        return unsafe{ self.mmap.offset(HEAP_SIZE as isize) } as *mut u8;
+    }
+}
+
+impl Drop for MemoryMap {
+    fn drop(&mut self) {
+        unsafe {
+            libc::munmap(self.mmap, (HEAP_SIZE + BLOCK_SIZE) as libc::size_t);
+        }
+    }
+}
+
 
 /// The `BlockAllocator` is the global resource for blocks for the immix
 /// space.
@@ -24,7 +76,7 @@ use gc_object::GCObjectRef;
 /// returned blocks first.
 pub struct BlockAllocator {
     /// The memory map of `HEAP_SIZE`.
-    mmap: os::MemoryMap,
+    mmap: MemoryMap,
 
     /// The pointer to the last allocated block.
     data: *mut u8,
@@ -42,23 +94,17 @@ impl BlockAllocator {
     /// This will `panic` if no memory map of size `HEAP_SIZE` can be
     /// allocared.
     pub fn new() -> BlockAllocator {
-        let mmap = os::MemoryMap::new(HEAP_SIZE + BLOCK_SIZE,
-                                      &[os::MapOption::MapReadable,
-                                        os::MapOption::MapWritable]).unwrap();
-        let data = unsafe{ mmap.data().offset((BLOCK_SIZE - (mmap.data() as usize) % BLOCK_SIZE) as isize) };
-        let data_bound = unsafe{ mmap.data().offset(mmap.len() as isize) };
-        debug!("Allocated heap {:p} of size {}, usable range: {:p} - {:p} (size {}, {} blocks)",
-                mmap.data(), mmap.len(), data, data_bound,
-                (data_bound as usize) - (data  as usize),
-                ((data_bound as usize) - (data  as usize)) / BLOCK_SIZE);
+        let mmap = MemoryMap::new();
+        let data = mmap.aligned();
+        let bound = mmap.bound();
         debug_assert!((data as usize) % BLOCK_SIZE == 0,
             "Allocated mmap {:p} is not aligned (offset {})",
             data, (data as usize) % BLOCK_SIZE);
         return BlockAllocator {
             mmap: mmap,
             data: data,
-            data_bound: data_bound,
-            free_blocks: Vec::with_capacity(HEAP_SIZE / BLOCK_SIZE),
+            data_bound: bound,
+            free_blocks: Vec::with_capacity(TOTAL_BLOCKS),
         };
     }
 
@@ -80,7 +126,7 @@ impl BlockAllocator {
 
     /// Return if an address is within the bounds of the memory map.
     pub fn is_in_space(&self, object: GCObjectRef) -> bool {
-        return self.mmap.data() < (object as *mut u8)
+        return self.mmap.start() < (object as *mut u8)
             && (object as *mut u8) < self.data_bound;
     }
 }
